71bd9d094d01583be15e6d1b4e395e04b8393ec1
Kentaro Kawano <kekawano@yahoo-corp.jp>
2016-02-01 23:46:31 +0900

    コミットログを取得する部分を実装
    ついでにコード整理

--- main.go
+++ main.go
@@ -1,12 +1,93 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/xanzy/go-gitlab"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
+
+func scanListAll(getList func(page int) (int, error)) error {
+
+	loop := true
+	for page := 1; loop; page++ {
+		lastPage, err := getList(page)
+		if err != nil {
+			return err
+		}
+		loop = (page < lastPage)
+	}
+
+	return nil
+}
+
+func listAllOwnedProjects(git *gitlab.Client) ([]*gitlab.Project, error) {
+
+	result := make([]*gitlab.Project, 0, 1000)
+
+	getProjects := func(page int) (int, error) {
+
+		lpOpt := &gitlab.ListProjectsOptions{}
+		lpOpt.Page = page
+		lpOpt.PerPage = 100
+
+		ps, res, err := git.Projects.ListOwnedProjects(lpOpt)
+		if err != nil {
+			return 0, err
+		}
+
+		if res.StatusCode != 200 {
+			return 0, fmt.Errorf("responce %d:%s", res.StatusCode, res.Status)
+		}
+
+		result = append(result, ps...)
+
+		// 負荷軽減のため
+		time.Sleep(100 * time.Millisecond)
+
+		return res.LastPage, nil
+	}
+
+	err := scanListAll(getProjects)
+
+	return result, err
+}
+
+func listCommitFromProject(git *gitlab.Client, proj *gitlab.Project, opt *gitlab.ListCommitsOptions) ([]*gitlab.Commit, error) {
+
+	result := make([]*gitlab.Commit, 0, 365*10)
+
+	pid := proj.ID
+	getCommits := func(page int) (int, error) {
+		opt := &gitlab.ListCommitsOptions{}
+		opt.Page = page
+		opt.PerPage = 100
+
+		cs, res, err := git.Commits.ListCommits(pid, opt)
+		if err != nil {
+			return 0, nil
+		}
+
+		if res.StatusCode != 200 {
+			return 0, fmt.Errorf("pid: %d, responce %d:%s\n", pid, res.StatusCode, res.Status)
+		}
+
+		result = append(result, cs...)
+
+		// 負荷軽減のため
+		time.Sleep(100 * time.Millisecond)
+
+		return res.LastPage, nil
+	}
+
+	err := scanListAll(getCommits)
+
+	return result, err
+
+}
 
 func main() {
 	token := os.Getenv("GITLAB_TOKEN")
@@ -30,16 +111,18 @@ func main() {
 		git.SetBaseURL(*baseURL)
 	}
 
-	ps, res, err := git.Projects.ListOwnedProjects(nil)
+	ps, err := listAllOwnedProjects(git)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if res.StatusCode != 200 {
-		log.Fatalf("responce %d:%s", res.StatusCode, res.Status)
-	}
-
+	cs := make([]*gitlab.Commit, 0, 365*10)
 	for _, p := range ps {
-		log.Print(p)
+		l, err := listCommitFromProject(git, p, nil)
+		if err != nil {
+			log.Printf("")
+			continue
+		}
+		cs = append(cs, l...)
 	}
 }
